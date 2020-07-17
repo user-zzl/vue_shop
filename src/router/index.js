e8613a5f75abceb83fd6b0da28b0225444bce111
zzl <309060079@qq.com>
2020-07-18 00:29:57 +0800

    完成用户列表功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,25 +2,32 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/home.vue'
+import Welcome from '../components/Welcome.vue'
+import User from '../components/user/User.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', redirect: '/login' },
-  { path: '/login', component: Login },
-  { path: '/home', component: Home }
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    {
+        path: '/home',
+        component: Home,
+        redirect: '/welcome',
+        children: [{ path: '/welcome', component: Welcome }, { path: '/users', component: User }]
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 // 挂载路由导航守卫
 router.beforeEach((to, from, next) => {
-  if (to.path === '/login') return next()
-  const tokenStr = window.sessionStorage.getItem('token')
-  if (!tokenStr) return next('/login')
-  next()
+    if (to.path === '/login') return next()
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login')
+    next()
 })
 
 export default router
